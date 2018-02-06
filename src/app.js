9ead9cc2f5f20c544a747ebe5ac2e9168eb330fc
pb3975 <metzwa.2@gmail.com>
2018-02-06 14:28:43 -0500

    First Commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is React.js BoilerPlate made by pb3975 </p>, document.getElementById('app'));
